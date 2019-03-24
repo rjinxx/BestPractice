15680703ee56e7cbf3630274c1adb8963cb871be
Ryan Jin <xiaojun.jin@outlook.com>
2019-03-24 17:07:39 +0800

    update

--- UITableView/BestPractice.swift
+++ UITableView/BestPractice.swift
@@ -6,5 +6,5 @@
 //  Copyright © 2019 Perfect365, Inc. All rights reserved.
 //
 
-// 用cellModel配置cell, 用viewModel配置tableView
+// 'ViewModelItem' for cell and 'ViewModel' for table view
 // https://medium.com/ios-os-x-development/ios-multiple-selections-in-table-view-88dc2249c3a2
